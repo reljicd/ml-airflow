86d5c00fb8b8f92a3e560ab6fc607544ac8688d8
Dušan Reljić <reljicd@gmail.com>
2018-01-19 14:31:20 +0100

    implemented helper script to facilitate building Docker image and running Docker container from that image

---
+++ scripts/run_docker.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME=reljicd/airflow-playground
+echo -e "\nSet docker image name as ${IMAGE_NAME}\n"
+PORT=8080
+echo -e "Set docker image PORT to ${PORT}\n"
+AIRFLOW_HOME=/airflow
+echo -e "Airflow home is set to ${AIRFLOW_HOME}\n"
+
+echo -e "\nStop running Docker containers with image name ${IMAGE_NAME}...\n"
+docker stop $(docker ps -a | grep ${IMAGE_NAME} | awk '{print $1}')
+
+echo -e "\nDocker build image with name ${IMAGE_NAME}...\n"
+docker build -t ${IMAGE_NAME} -f docker/Dockerfile .
+
+echo -e "\nStart Docker container of the image ${IMAGE_NAME}...\n"
+docker run --rm -i \
+    -p ${PORT}:${PORT} \
+    -v dags:${AIRFLOW_HOME}/dags \
+    ${IMAGE_NAME}
