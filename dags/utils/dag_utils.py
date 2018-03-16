d65fc13a8ff2e6d8f637653881076766e23cf636
Dušan Reljić <reljicd@gmail.com>
2018-03-16 18:27:25 +0100

    added dag_utils module

---
+++ dags/utils/dag_utils.py
@@ -0,0 +1,18 @@
+def get_ml_dag_id(parent_dag_id: str, **kwargs) -> str:
+    """ Extracts ml_dag_id either from kwargs or from XCom
+
+    Args:
+        parent_dag_id:
+        **kwargs:
+
+    Returns: ml_dag_id
+
+    """
+    if 'ml_dag_id' in kwargs:
+        ml_dag_id = kwargs['ml_dag_id']
+    else:
+        ml_dag_id = kwargs['task_instance'].xcom_pull(task_ids='initializer',
+                                                      key='ml_dag_id',
+                                                      dag_id=parent_dag_id)
+
+    return ml_dag_id
