4b30331dc9295fe1051d79ff1324a88e124b85a5
Dušan Reljić <reljicd@gmail.com>
2018-03-16 18:19:24 +0100

    made new version of run_docker.sh script

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,20 +1,29 @@
 #!/usr/bin/env bash
 
-IMAGE_NAME=reljicd/airflow-playground
+CONTAINER_NAME=reljicd-airflow
+echo -e "\nSet docker container name as ${CONTAINER_NAME}\n"
+IMAGE_NAME=${CONTAINER_NAME}:reljicd
 echo -e "\nSet docker image name as ${IMAGE_NAME}\n"
 PORT=8080
 echo -e "Set docker image PORT to ${PORT}\n"
 AIRFLOW_HOME=/airflow
 echo -e "Airflow home is set to ${AIRFLOW_HOME}\n"
 
-echo -e "\nStop running Docker containers with image name ${IMAGE_NAME}...\n"
-docker stop $(docker ps -a | grep ${IMAGE_NAME} | awk '{print $1}')
+echo -e "\nStop running Docker containers with image tag ${CONTAINER_NAME}...\n"
+docker stop $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')
+
+chmod +x docker/initialize_airflow.sh
 
 echo -e "\nDocker build image with name ${IMAGE_NAME}...\n"
 docker build -t ${IMAGE_NAME} -f docker/Dockerfile .
 
-echo -e "\nStart Docker container of the image ${IMAGE_NAME}...\n"
+echo -e "\nStart Docker container of the image ${IMAGE_NAME} with name ${CONTAINER_NAME}...\n"
 docker run --rm -i \
     -p ${PORT}:${PORT} \
-    -v dags:${AIRFLOW_HOME}/dags \
+    -e DB_HOST=${DB_HOST} \
+    -e DB_LOGIN=${DB_LOGIN} \
+    -e DB_PASSWORD=${DB_PASSWORD} \
+    -e DB_SCHEMA=${DB_SCHEMA} \
+    -e DB_CONN_ID=${DB_CONN_ID} \
+    --name ${CONTAINER_NAME} \
     ${IMAGE_NAME}
