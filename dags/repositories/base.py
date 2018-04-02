e889efc1f12b92a3d52ccaab53a78d1d861b4ab6
Dušan Reljić <reljicd@gmail.com>
2018-04-02 11:45:08 +0200

    engine creation fix

--- dags/repositories/base.py
+++ dags/repositories/base.py
@@ -1,5 +1,9 @@
+from airflow.hooks.base_hook import BaseHook
 from sqlalchemy import MetaData
 from sqlalchemy.engine.base import Engine
+
+from dags.config import settings
+from dags.utils import db_utils
 
 
 class BaseRepository(object):
@@ -9,4 +13,14 @@ class BaseRepository(object):
     def __init__(self, engine: Engine):
         # Bind engine to metadata only if it is not bind already
         if self.metadata.bind is None:
+            # Skip engine building if it is already provided
+            if engine is None:
+                connection = BaseHook.get_connection(conn_id=settings.DB_CONN_ID)
+
+                engine = db_utils.create_db_engine(login=connection.login,
+                                                   password=connection.password,
+                                                   host=connection.host,
+                                                   schema=connection.schema,
+                                                   conn_type=connection.conn_type)
+
             self.metadata.bind = engine
